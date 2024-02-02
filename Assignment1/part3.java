c30e04a304461c2a952bba99dbc13b60dd4522b4
John Skinner <jjcokeky@gmail.com>
2024-02-02 09:49:51 -0500

    Added Part 3

--- Assignment1/part3.java
+++ Assignment1/part3.java
@@ -1,8 +1,32 @@
-public class part2 {
+public class part3 {
     public static void main(String[] args) {
 
-        char letter;
-        short grade;
+        char letter = 'C';
+        short grade = 30;
 
+        if (grade >= 90 && grade <= 100) {
+            letter = 'A';
+        }
+        if (grade >= 80 && grade <= 89) {
+            letter = 'B';
+        }
+        if (grade >= 70 && grade <= 79) {
+            letter = 'C';
+        }
+        if (grade >= 60 && grade <= 69) {
+            letter = 'D';
+        }
+        if (grade >= 50 && grade <= 59) {
+            letter = 'E';
+        }
+        if (grade >= 0 && grade <= 49) {
+            letter = 'F';
+        }
+        System.out.println(letter);
+
+        if (letter == 'F') {
+            System.out.println("Failed");
+        } else
+            System.out.println("Passed");
     }
 }
