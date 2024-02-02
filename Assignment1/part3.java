f691ed821c0f28c3c4f61828dc9581b361405c0a
John Skinner <jjcokeky@gmail.com>
2024-02-02 09:57:20 -0500

    if else
    Added my if elses

--- Assignment1/part3.java
+++ Assignment1/part3.java
@@ -1,26 +1,24 @@
 public class part3 {
     public static void main(String[] args) {
 
+        // Add Scanner funtion
         char letter = 'C';
-        short grade = 30;
+        short grade = 1000;
 
         if (grade >= 90 && grade <= 100) {
             letter = 'A';
-        }
-        if (grade >= 80 && grade <= 89) {
+        } else if (grade >= 80 && grade <= 89) {
             letter = 'B';
-        }
-        if (grade >= 70 && grade <= 79) {
+        } else if (grade >= 70 && grade <= 79) {
             letter = 'C';
-        }
-        if (grade >= 60 && grade <= 69) {
+        } else if (grade >= 60 && grade <= 69) {
             letter = 'D';
-        }
-        if (grade >= 50 && grade <= 59) {
+        } else if (grade >= 50 && grade <= 59) {
             letter = 'E';
-        }
-        if (grade >= 0 && grade <= 49) {
+        } else if (grade >= 0 && grade <= 49) {
             letter = 'F';
+        } else {
+            System.out.println("wrong input");
         }
         System.out.println(letter);
 
