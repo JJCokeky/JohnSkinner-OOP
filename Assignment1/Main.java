1ff355984460b36f9b02067014c3fe0b9e8297c4
John Skinner <jjcokeky@gmail.com>
2024-02-07 12:46:55 -0500

    Moved files

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -5,19 +5,12 @@ John Skinner
 
 public class Main {
     public static void main(String[] args) {
-        printStuff();
-    }
-
-    // <editor-fold desc="Print John's Stuff">
-    private static void printStuff() {
         System.out.println(
-                """
-                         John Skinner - Computer Systems Technician - Transfer to Ontario Tech University Bachelor of Information Technology (Hons)
-                        (    Professor - Yuvaraj Sambandan)
-                        (    COSC-1200-06 Object-Oriented Programming 1)
-                        (    Tusdays 11:10am - 1:00pm   Wendesys 12:10pm - 2:00pm   Friday 8:10am - 10:00am)
-                        (    Room SW215                 Room A317                   Room SW110
-                                """);
+                "John Skinner - Computer Systems Technician - Transfer to Ontario Tech University Bachelor of Information Technology (Hons");
+        System.out.println("Professor - Yuvaraj Sambandan");
+        System.out.println("COSC-1200-06 Object-Oriented Programming 1");
+        System.out.println("Tusdays 11:10am - 1:00pm   Wendesys 12:10pm - 2:00pm   Friday 8:10am - 10:00am");
+        System.out.println("Room SW215                 Room A317                   Room SW110");
+
     }
-    // </editor-fold>
 }
