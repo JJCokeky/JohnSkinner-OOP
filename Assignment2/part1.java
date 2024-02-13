40162acbc718fc02759e3b056f1a67c12c1a4129
John Skinner <jjcokeky@gmail.com>
2024-02-13 11:54:14 -0500

    Added part1 for A2
    I do not know what the other xml files are doing

--- Assignment2/part1.java
+++ Assignment2/part1.java
@@ -4,11 +4,17 @@
  */
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class part1 {
     public static void main(String[] args) {
 
-        int[] Numbers = new int[8];
+        int[] Numbers = new int[8]; // create the array to 8 digets
+        Scanner Input = new Scanner(System.in); // create the scanner
 
+        for (int i : Numbers) { // ask for the numbers and check them for errors
+            System.out.println(i);
+
+        }
     }
 }
