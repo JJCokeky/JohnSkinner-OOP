435bddd641913f40da2547c7f38bbf359ea38c29
John Skinner <jjcokeky@gmail.com>
2024-02-15 14:04:36 -0500

    basic of what I need to do

---
+++ Assignment2/part1.java
@@ -0,0 +1,34 @@
+/*
+ John Skinner
+ 2024-02-15
+ */
+
+import java.util.Scanner;
+
+public class part1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); // create the scanner named input
+
+        int[] scores = new int[8]; // create the scores array
+
+
+        for (int i = 0; i < scores.length; i++) { //for each number in the score arrays 
+            boolean correctInput = false; // set the error checking to false because each input has not been checked 
+
+            while (!correctInput) { // error checking 
+                try {
+
+
+
+
+
+                }
+            }
+        }
+
+
+
+
+    }
+}
