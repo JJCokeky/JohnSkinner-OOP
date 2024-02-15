a43ba633f5357fd9b67ea08d678f940d4925d7f3
John Skinner <jjcokeky@gmail.com>
2024-02-15 13:05:06 -0500

    Got input into array

--- Assignment2/part1.java
+++ Assignment2/part1.java
@@ -3,18 +3,35 @@
  2024-02-09
  */
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class part1 {
     public static void main(String[] args) {
 
-        int[] Numbers = new int[8]; // create the array to 8 digets
-        Scanner Input = new Scanner(System.in); // create the scanner
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[8];
 
-        for (int i : Numbers) { // ask for the numbers and check them for errors
-            System.out.println(i);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Please eneter score: ");
+            numbers[i] = input.nextInt();
+            // try { numbers[i] >= 0 || <=100};
 
         }
+        input.close();
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+        System.out.println(numbers[5]);
+        System.out.println(numbers[6]);
+        System.out.println(numbers[7]);
     }
 }
+
+// int[] Numbers = new int[8]; // create the array to 8 digets
+// Scanner Input = new Scanner(System.in); // create the scanner
+// int Numbers[] = new int[8];
+
+// for (int i : Numbers) { // ask for the numbers and check them for errors
+// System.out.println(i);
