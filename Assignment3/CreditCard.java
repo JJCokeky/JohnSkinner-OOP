663413d96bf5fb2f28441b1f417386361557943e
John Skinner <jjcokeky@gmail.com>
2024-03-04 21:09:00 -0500

    assignment 3

--- Assignment3/CreditCard.java
+++ Assignment3/CreditCard.java
@@ -1,3 +1,7 @@
+// John Skinner
+// 2024-02-23
+// Object- oriented programing
+
 package Assignment3;
 
 public class CreditCard {
@@ -5,27 +9,26 @@ public class CreditCard {
     private int month;
     private int year;
     private String holder;
-}
 
-    public CreditCard(long accountNumber, int month, int year, String holder) {
-        long accountNumber1 = accountNumber;
-        int month1 = month;
-        int year1 = year;
-        String holder1 = holder;
+    public CreditCard(long accountNumber1, int month1, int year1, String holder1) {
+        long accountNumber = accountNumber1;
+        int month = month1;
+        int year = year1;
+        String holder = holder1;
     }
 
-    public CreditCard() {
-
-    private long DEFAULT_ACCOUNT_NUMBER = 1234567890123456L;
-    private int DEFULT_MONTH = 1;
-    private int DEFAULT_YEAR = 99;
-    private String DEFAULT_NAME = "Bob Loblaws";
+    public CreditCard() { // create the defult values
+        long DEFAULT_ACCOUNT_NUMBER = 1234567890123456L;
+        int DEFULT_MONTH = 1;
+        int DEFAULT_YEAR = 99;
+        String DEFAULT_NAME = "Bob Loblaws";
     }
 
     // display card info
-public void displayCardInfo() {
-    System.out.println("Your Account Number is " + accountNumber);
-    System.out.println("Month is " + month);
-    System.out.println("Year is " + year);
-    System.out.println("Name is  " + holder);
+    public void displayCardInfo() {
+        System.out.println("Your Account Number is " + accountNumber);
+        System.out.println("Month is " + month);
+        System.out.println("Year is " + year);
+        System.out.println("Name is  " + holder);
+    }
 }
--- Assignment3/CreditCardTester.java
+++ Assignment3/CreditCardTester.java
@@ -1,11 +1,14 @@
+// John Skinner
+// 2024-02-23
+// Object- oriented programing
+
 package Assignment3;
 
 public class CreditCardTester {
-    public static void main(String[] args) {
+    public static void creditcardtester(String[] args) {
         CreditCard MyObject1 = new CreditCard(); // no passed arguments so display defult
-        CreditCard MyObject2 = new CreditCard(5312264554231345L, 1, 10, "Jane Smith"); // The second one should be
-                                                                                       // passed the values
-        CreditCard MyObject3 = new CreditCard(10234, 5, 8, "Neva Read"); // setter method
+        CreditCard MyObject2 = new CreditCard(5312264554231345L, 1, 10, "Jane Smith"); // valid account number
+        CreditCard MyObject3 = new CreditCard(10234, 5, 8, "Neva Read"); // invaild account number
 
         // getter to display each variable
         MyObject1.displayCardInfo();
