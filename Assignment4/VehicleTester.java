79516c3c3b3168365725c6441cb0873298c4b3d5
John Skinner <jjcokeky@gmail.com>
2024-03-28 20:24:36 -0400

    VehicleTester

--- Assignment4/VehicleTester.java
+++ Assignment4/VehicleTester.java
@@ -1,3 +1,10 @@
+/*
+John Skinner
+2024-03-25
+This program will set, get, and display diffrent types of vehicles.
+
+This class extends vehicle to add engine size and engine type 
+ */
 package Assignment4;
 
 public class VehicleTester {
@@ -7,8 +14,17 @@ public class VehicleTester {
         Motorcycle Motorcycle = new Motorcycle("Honda", "Shadow", 2004, "JH2RC040XBM200124", 750, "4 Stroke");
         Automobile Automobile = new Automobile("Chevy", "Corvette", 1975, "1G4EDYR2XAS123456", 254, "unleaded");
 
-        Vehicle.getVehicleInfo();
-        Motorcycle.getVehicleInfo();
-        Automobile.getVehicleInfo();
+        System.out.println(Vehicle.getVehicleInfo());
+        System.out.println(Motorcycle.getVehicleInfo());
+        System.out.println(Automobile.getVehicleInfo());
+
+        // Made the array with the diffrent vehicle types
+        Vehicle[] vehicles = { Vehicle, Automobile, Motorcycle };
+
+        // itterate through the vehicle types using get vehicle info
+        for (Vehicle i : vehicles) {
+            System.out.println(i.getVehicleInfo());
+        }
+
     }
 }
