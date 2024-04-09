f2851fc82ca7c0e61392cf1a337e28971efd3642
John Skinner <jjcokeky@gmail.com>
2024-04-09 12:02:23 -0400

    example from class and started on last in class

---
+++ LastInClass/GUI.java
@@ -0,0 +1,19 @@
+package LastInClass;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private static JLabel titleLogin;
+    private static JLabel Username;
+    private static JTextField username;
+    private static JLabel Password;
+    private static JTextField password;
+    private static JButton submitButton;
+
+    // might not be needed
+    // private static JButton exitButton;
+}
